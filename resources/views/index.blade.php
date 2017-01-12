09e60362a39c26d0d79bb9e46b245ed41175e2eb
AvengerWeb <i@avenger-web.ru>
2017-01-12 18:19:11 +0300

    Add favicons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,17 @@
 
     <meta name="_token" content="<?=Crypt::encrypt(csrf_token()) ?>">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="favicons/manifest.json">
+    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#ae5bd5">
+    <link rel="shortcut icon" href="favicons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="AvengerWeb">
+    <meta name="application-name" content="AvengerWeb">
+    <meta name="msapplication-config" content="favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <title>{{ Config::get("website.title") }}</title>
 </head>
 <body>
