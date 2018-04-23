219cb13c92f983a05c9e5f7421e3ac1cae4b4e15
avengerweb <avengerweb@yandex.ru>
2018-04-24 01:40:52 +0300

    Some little work

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,18 @@
 @section('content')
     <div class="container">
         <h1>@lang('Latest blog posts')</h1>
-        <div class="card">
-            <div class="card-header">Posts</div>
+        <div class="row">
+            @foreach([1, 2, 3, 4] as $item)
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">Posts</div>
 
-            <div class="card-body">
-                one two
-            </div>
+                        <div class="card-body">
+                            one two
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
