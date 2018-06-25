2e798335f414fd01338e0d9c29d4a7522758fff2
avengerweb <avengerweb@yandex.ru>
2018-06-26 00:21:36 +0300

    CORS

---
+++ app/Http/Middleware/CORSAccess.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * Class CORSAccess
+ *
+ * Add CORS headers to all requests
+ *
+ * @package App\Http\Middleware
+ */
+class CORSAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
+     * @return mixed
+     * @throws \Illuminate\Support\Facades\SuspiciousOperationException
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (!starts_with($request->getRequestUri(), "/api/") && $request->getHost() === 'api.sirus.su')
+        {
+            throw new NotFoundHttpException();
+        }
+
+        $response = $request->method() === 'OPTIONS' ? response('') : $next($request);
+
+        if ($origin = $request->headers->get('referer', $request->headers->get('origin')))
+        {
+            $origin = parse_url($origin);
+
+            if (isset($origin['scheme']) && isset($origin['host'])) {
+                $origin = $origin['scheme'] . "://" . $origin['host'] . (isset($origin['port']) ? ":" . $origin['port'] : "");
+            } else {
+                $origin = '*';
+            }
+        }
+        else
+            $origin = "*";
+
+        $response->headers->set('Access-Control-Allow-Origin', $origin);
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE, HEAD');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-atlassian-mau-ignore");
+
+        return $response;
+    }
+}
